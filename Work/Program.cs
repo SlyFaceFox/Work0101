9cf713797eae1459ab48e4f17edbff0c89691e44
Huawei <Huawei@LAPTOP-5LRHA01L>
2022-09-25 07:34:46 +0300

    Добавьте файлы проекта.

---
+++ Work/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Mainclass
+{
+    static void Main()
+    {
+        double R, r;
+        Console.WriteLine("Введите значение R:");
+        R = int.Parse (Console.ReadLine());
+        Console.WriteLine("Введите значение r:");
+        r = int.Parse (Console.ReadLine());
+        double S = Math.PI * ((Math.Pow(R, 2) - (Math.Pow(r, 2))));
+        Console.WriteLine("Ответ: {0}", S);
+    }
+}
